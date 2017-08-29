9dba5cf58e61fa87c9704e1359772d814f460cf3
ishaanSejwal <ishaan.sejwal@gmail.com>
2017-08-29 16:57:33 +0530

    imporved manifest

--- Package.swift
+++ Package.swift
@@ -3,5 +3,10 @@
 import PackageDescription
 
 let package = Package(
-    name: "Clibgit2"
+    name: "Clibgit2",
+    pkgConfig: "libgit2",
+    providers: [
+        .Brew("libgit2")
+        .apt("libgit2-0")
+    ]
 )
