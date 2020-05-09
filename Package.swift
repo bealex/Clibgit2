bde2b43c010614c4ec2c49da1f824986bb78d289
Alex Babaev <alex@jdnevnik.com>
2020-05-09 14:51:09 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,11 +1,13 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 
 import PackageDescription
 
 let package = Package(
     name: "Clibgit2",
-    pkgConfig: "libgit2",
-    providers: [
-        .brew(["libgit2"])
+    products: [
+        .library(name: "Clibgit2", targets: [ "Clibgit2" ])
+    ],
+    targets: [
+        .systemLibrary(name: "Clibgit2", pkgConfig: "libgit2", providers: [ .brew(["libgit2"]) ])
     ]
 )
