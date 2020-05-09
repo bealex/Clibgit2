bbbc1eb564326e8bde4968309f25c059d2d83216
Alex Babaev <alex@devpocket.com>
2020-05-09 19:22:53 +0300

    Working version of the library

--- Package.swift
+++ Package.swift
@@ -1,13 +1,17 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 
 import PackageDescription
 
 let package = Package(
     name: "Clibgit2",
     products: [
-        .library(name: "Clibgit2", targets: [ "Clibgit2" ])
+    	.library(name: "Clibgit2", targets: [ "Clibgit2" ])
     ],
     targets: [
-        .systemLibrary(name: "Clibgit2", pkgConfig: "libgit2", providers: [ .brew(["libgit2"]) ])
+    	.systemLibrary(
+    		name: "Clibgit2",
+    		pkgConfig: "libgit2",
+    		providers: [ .brew([ "libgit2" ]) ]
+    	)
     ]
 )
